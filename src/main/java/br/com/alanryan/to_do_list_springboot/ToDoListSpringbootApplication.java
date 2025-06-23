680b4b759cf943a425650f50201fe302459604e6
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-06-23 20:01:45 -0300

    chore: Created a new project and carried out the scaffolding increment

---
+++ src/main/java/br/com/alanryan/to_do_list_springboot/ToDoListSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.alanryan.to_do_list_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alanryan/to_do_list_springboot/ToDoListSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alanryan.to_do_list_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
