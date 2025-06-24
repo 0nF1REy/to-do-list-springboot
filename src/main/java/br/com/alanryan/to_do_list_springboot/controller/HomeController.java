fb6490a2b8b50dbe6f90811b41297dc5ad6a46f2
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-06-24 00:01:45 -0300

    feat: Add "HomeController" with root and ping endpoints for basic health check

--- src/main/java/br/com/alanryan/to_do_list_springboot/controller/HomeController.java
+++ src/main/java/br/com/alanryan/to_do_list_springboot/controller/HomeController.java
@@ -1,4 +1,24 @@
 package br.com.alanryan.to_do_list_springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, String> home() {
+        return Map.of(
+                "message", "To Do List is running!",
+                "version", "1.0.0",
+                "status", "OK"
+        );
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
 }
